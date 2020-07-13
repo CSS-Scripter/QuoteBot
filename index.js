91d3c0305e48989eb8a205bb7d554ce7f11ecb8e
Thom Leenman <thomleenman@gmail.com>
2020-07-13 13:03:50 +0200

    It now uploads to the server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js')
-const { prefix, token } = require('./config.json')
+const { prefix, hostname, token } = require('./config.json')
+const http = require('http')
+
 const client = new Discord.Client()
 
 client.once('ready', () => {
@@ -8,13 +10,13 @@ client.once('ready', () => {
 
 client.on('message', message => {
     if (message.content.startsWith(`${prefix}quote`)) {
-        quoteIt()
+        quoteIt(message)
     }
 })
 
 function quoteIt(message) {
     let quote = message.content.substring(7)
-    let quotedPerson = message.mentions.members.last()
+    let quotedPerson = message.mentions.members.last().user.username
 
     if (quotedPerson === undefined || !quote.endsWith('>')) {
         message.channel.send('Who are you quoting? Mention them at the end of the quote!')
@@ -22,7 +24,43 @@ function quoteIt(message) {
     }
 
     quote = quote.substring(0, quote.lastIndexOf('<'))
-    message.channel.send(`Quoted: ${quote} - ${quotedPerson} 2020`)
+
+    const url = 'localhost:3000'
+    const body = JSON.stringify({
+        message: quote,
+        by: quotedPerson,
+        year: '2000'
+    })
+
+    const options = {
+        hostname: hostname,
+        port: 80,
+        path: '/v1/new',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': body.length
+        }
+    }
+
+    const request = http.request(options, (res) => {
+        console.log(`Statuscode: ${res.statusCode}`)
+        console.dir(res)
+
+        res.on('data', (d) => {
+            console.log(d)
+        })
+
+        message.channel.send(`Quoted: ${quote} - ${quotedPerson} 2020`)
+    })
+
+    request.on('error', (e) => {
+        console.error(e)
+    })
+
+    request.write(body)
+    request.end()
+
 }
 
 client.login(token);
